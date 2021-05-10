80d626245c02c4fc0ccf26062eed41325ceb1409
Carmen D <carmengdominguez94@gmail.com>
2021-05-10 16:41:40 +0200

    history control

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            XIsNext: true,
-        };
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
         
@@ -36,24 +28,14 @@ class Board extends React.Component {
 
     renderSquare(i) {
         return <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />;
     }
 
     render() {
-        let status = 'Next player: ' + (this.state.XIsNext ? 'X': 'O');
-        const winner = calculateWinner(this.state.squares);
-
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.XIsNext ? 'X': 'O'); 
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -75,15 +57,75 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            XIsNext: true,
+            stepNumber: 0,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.XIsNext ? 'X': 'O';
+
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            XIsNext: !this.state.XIsNext,
+            stepNumber: history.length,
+        })
+    }
+
+    jumpTo(step) {
+        this.setState({
+            XIsNext: (step % 2) === 0,
+            stepNumber: step,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        })
+
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.XIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -92,12 +134,13 @@ class Game extends React.Component {
 
 function calculateWinner(squares) {
     const lines = [
-        [0,1,2],
         [3,4,5],
+        [0,1,2],
         [6,7,8],
         [0,3,6],
         [1,4,7],
         [2,5,8],
+        [0,4,8],
         [2,4,6],
     ];
 
@@ -107,8 +150,8 @@ function calculateWinner(squares) {
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
-        return null;
     }
+    return null;
 }
 
 // ========================================
